c801aad8fc4390ef7a5ceadf1047ce002b3dae75
Dee Race <deerace12@gmail.com>
2018-08-20 16:42:28 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,17 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.post "https://github.com/login/oauth/access_token" do |req|
-      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
-      req.params['client_secret'] = ENV['GITHUBT_CLIENT_SECRET']
-      req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
-      req.params['code'] = params[:code]
       req.body = { 'client_id': ENV['GITHUB_CLIENT_ID'], 'client_secret': ENV['GITHUBT_CLIENT_SECRET'], 'code': params[:code] }
       req.headers['Accept'] = 'application/json'
+    end 
 
       body = JSON.parse(resp.body)
       session[:token] = body["access_token"]
+      user_info = Faraday.get("https://api.github.com/user") do |req| 
+        req.headers = {'Accept': 'application.json', 'Authorization': 'token #{session[:token]}'}
+      end
+      user = JSON.parse(user_info.body)
+      session[:username] = user["login"]
       redirect_to root_path
     end
   end
